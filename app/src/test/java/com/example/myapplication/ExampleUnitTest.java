35763f51c910d6ac6c686c35ec61059e14962f20
Tanmay Kulkarni <tanmay.n.kulkarni@hotmail.com>
2022-02-06 14:59:55 -0800

    bugs

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
@@ -10,6 +13,16 @@ import android.util.Log;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.myapplication.student.database.AppDatabaseCourses;
+import com.example.myapplication.student.database.Course;
+import com.example.myapplication.student.database.CourseDao;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,10 +31,43 @@ import androidx.appcompat.app.AppCompatActivity;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 
+@RunWith(AndroidJUnit4.class)
 public class ExampleUnitTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void test_addClasses() {
+        AppDatabaseCourses dbCourse;
+
+        dbCourse = AppDatabaseCourses.singleton(getApplicationContext());
+
+        CourseDao courseDao = dbCourse.courseDao();
+        courseDao.insertCourse(
+                new Course(
+                        1,
+                        String.valueOf("2020"),
+                        String.valueOf("FA"),
+                        "110"
+                ));
+
+        List<Course> actual = courseDao.getAllCourses();
+
+        List<Course> expected = new ArrayList<>();
+        expected.add(
+                new Course(
+                        1,
+                        String.valueOf("2020"),
+                        String.valueOf("FA"),
+                        "110"
+                        ));
+
+        assertArrayEquals(expected.toArray(), actual.toArray());
     }
     
 }
